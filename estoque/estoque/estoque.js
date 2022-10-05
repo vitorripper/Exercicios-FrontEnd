632401f2c1525f210a2e4107d754c1281096e087
Vitor Ripper <88108629+vitorripper@users.noreply.github.com>
2022-10-05 07:36:35 -0300

    AVALIAÇÃO EX2

---
+++ estoque/estoque/estoque.js
@@ -0,0 +1,58 @@
+const produto = ['Celular', 'Notebook', 'Teclado', 'Mouse', 'Monitor', 'Mousepad',
+                'Óculos', 'Impressora', 'Câmera', 'Fone']
+const estoque = [Math.random()*100, Math.random()*100, Math.random()*100,
+    Math.random()*100, Math.random()*100, Math.random()*100, Math.random()*100,
+    Math.random()*100, Math.random()*100, Math.random()*100, Math.random()*100];
+const preco = [20.99, 1, 42, 69.12, 911.43, 2808.20, 2807.8, 7, 1200.50,13 ,22 ];
+
+function botao(){
+    
+    var res = document.querySelector('.ps');
+    var res1 = document.querySelector('.vp');
+    var res2 = document.querySelector(".vt");
+    var res3 = document.querySelector(".es");
+
+   
+    var quantidade= document.querySelector(".qtd").value;
+    var posicao= document.querySelector(".produtos").value;
+    
+    valorTotal = quantidade*preco[posicao]
+
+   
+    if (quantidade <= estoque[posicao]) {
+        res.innerHTML = `Produdto selecionado: ${produto[posicao]}`
+
+        res3.innerHTML = `Produtos em Estoque: ${parseInt(estoque[posicao])}`
+
+        res2.innerHTML = `Valor Total da Compra: ${valorTotal.toLocaleString("pt-BR", 
+        { style: "currency" , currency:"BRL"})}`;
+
+        res1.innerHTML = `Valor do produto: ${preco[posicao].toLocaleString("pt-BR", 
+        { style: "currency" , currency:"BRL"})}`
+    }
+    else if(quantidade > estoque[posicao]){
+        alert('PRODUTO FORA DE ESTOQUE')
+    }
+} 
+
+
+
+
+
+/*
+⢀⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀⢀⢴⣶⣆ 
+⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠁⠸⣼⡿ 
+⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉⠀⠀⠀⠀⠀ 
+⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉
+*/
